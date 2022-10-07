e41301cfebdfa40fdda71cccac43a190e7b0e318
jungh8n <jh981113@naver.com>
2022-10-07 15:10:01 +0900

    init commit

---
+++ src/main/java/com/sdsgsg/tastyzipbackend/TastyZipBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sdsgsg.tastyzipbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyZipBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyZipBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdsgsg/tastyzipbackend/TastyZipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdsgsg.tastyzipbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyZipBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
