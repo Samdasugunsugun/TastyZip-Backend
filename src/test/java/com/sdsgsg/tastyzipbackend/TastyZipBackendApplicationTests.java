647aa961ee76a4ae34df4f30c18cb8268c297794
jungh8n <jh981113@naver.com>
2022-12-09 18:37:22 +0900

    fix: delete Test Code

--- src/test/java/com/sdsgsg/tastyzipbackend/TastyZipBackendApplicationTests.java
+++ src/test/java/com/sdsgsg/tastyzipbackend/TastyZipBackendApplicationTests.java
@@ -1,13 +1,8 @@
 package com.sdsgsg.tastyzipbackend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TastyZipBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
